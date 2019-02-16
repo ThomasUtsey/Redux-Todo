da6c42e538cdb8ba3b98101d61813d12177607e1
thomasutsey <tomutsey@yahoo.com>
2019-02-16 11:57:26 -0500

    restructure app component to handle flow from components
    add connect import
    
    add components import
    
    add map to props function

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
 import TodoForm from './components/toDoForm';
-import TodoList from "./TodoList";
+import TodoList from "./components/todolist";
 import {connect} from "react-redux"
 
 
 class App extends Component {
+  constructor (props){
+    super (props);
+    this.state={
+      listName:'',
+    }
+  }
+  addTodo = (input)=>{
+    let newTodo ={
+      todo: input,
+      completed: false
+    }
+
+  }
   render() {
     return (
       <div className="App">
-  
+      <h1>{this.props.listName}</h1>
+      <TodoList/>
+      <TodoForm/>
       </div>
     );
   }
 }
 
-export default App;
+function mstp(state){
+  return{listName: state.title }
+}
+
+export default connect(mstp)(App)
---
+++ todo/src/components/todolist.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+
+const TodoList = () =>{
+  return(<div>This will be my TodoList component</div>)
+}
+export default TodoList
